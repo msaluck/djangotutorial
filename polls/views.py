0436f6d99cfb67654beb63822967892ca7211db7
pawitran413 <aminbagus88@gmail.com>
2024-12-26 14:41:41 +0700

    H1A022032 - Update views.py

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from django.views import generic
 
 from .models import Choice, Question
 
+
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
@@ -14,17 +15,17 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by("-pub_date")[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
+
 def vote(request, question_id):
-    model = Question
-    template_name = "polls/results.html"
-    
     # same as above, no changes needed.
     ...
