2e442d3c8ad8482e12cb269b5b5aba570d75a22c
pawitran413 <aminbagus88@gmail.com>
2024-12-26 14:47:17 +0700

    H1A022032 - Memperbaiki fungsi vote pada views.py

--- polls/views.py
+++ polls/views.py
@@ -25,7 +25,24 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
-
 def vote(request, question_id):
-    # same as above, no changes needed.
-    ...
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
